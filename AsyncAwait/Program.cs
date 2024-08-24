e2d43db5c0595f94ce30dcb974d79fa4c5442a2a
swale-coder <swalesiddiqui505@gmail.com>
2024-08-24 17:58:55 +0530

    Add project files.

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo();
+            helloworld.hi();
+            Console.ReadKey();
+        }
+
+        public static async void Demo()
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            //await StartSchoolAssembly();
+
+
+            //StartSchoolAssembly();
+            //TeachClass12();
+            //TeachClass11();
+
+            Task.WaitAll(StartSchoolAssembly(), TeachClass11(), TeachClass12());
+
+
+            watch.Stop();
+            Console.WriteLine($"Execution Time: { watch.ElapsedMilliseconds} ms");
+        }
+
+        public static async Task StartSchoolAssembly()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Assembly Started");
+                Thread.Sleep(8000);
+                Console.WriteLine("Assemply Ended");
+            });
+        }
+
+
+        public static async Task TeachClass12()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Class 12 started");
+                Thread.Sleep(3000);
+                Console.WriteLine("Class 12 ended");
+            });
+
+        }
+
+        public static async Task TeachClass11()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Class 11 started");
+                Thread.Sleep(2000);
+                Console.WriteLine("Class 11 ended");
+            });
+
+        }
+    }
+}
---
+++ AsyncAwait/helloworld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    internal class helloworld
+    {
+        public static void hi()
+        {
+            Console.WriteLine("hi");
+            Console.WriteLine("hi");
+        }
+    }
+}
